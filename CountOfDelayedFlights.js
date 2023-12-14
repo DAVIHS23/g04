83eb4cedf36165cbfc44de40e52cd92edf8014b6
Nilukzil <35670802+Nilukzil@users.noreply.github.com>
2023-12-14 17:13:49 +0100

    Update CountOfDelayedFlights.js

--- CountOfDelayedFlights.js
+++ CountOfDelayedFlights.js
@@ -1,49 +1,44 @@
-var margin = {
-        top: 30,
-        right: 30,
-        bottom: 70,
-        left: 60
-    },
-    width = 300 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+const margin = {
+    top: 30,
+    right: 30,
+    bottom: 70,
+    left: 60
+};
+
+const width = 200 - margin.left - margin.right;
+const height = 200 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#kpi_nilu4")
+const svg = d3.select("#kpi_nilu4")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 svg.append("rect")
     .attr("width", width)
     .attr("height", height)
-    .attr("fill", "#f2f2f2"); 
+    .attr("fill", "#f2f2f2");
+
 // Parse the Data
-d3.csv("Merged_CSV.csv", function (data) {
-    
-     var filteredData = data.filter(function(d) {
-        return d.DELAY_IN_MIN >0; // Adjust the condition as needed
-    });
-    
-var totalCount = "Nr of Delayed Flights" + filteredData.length;;
+d3.csv("Merged_CSV.csv").then(data => {
+
+    const filteredData = data.filter(d => d.DELAY_IN_MIN > 0); // Adjust the condition as needed
+
+    const totalCount = "Nr of Delayed Flights:\n" + filteredData.length;
 
     // X axis
-    var x = d3.scaleBand()
+    const x = d3.scaleBand()
         .range([0, width])
         .domain(["DELAY_IN_MIN"]) // Only one value in the domain
         .padding(0.2);
 
-
-
     // Bars
-   svg.append("text")
+    svg.append("text")
         .attr("x", x("DELAY_IN_MIN") + x.bandwidth() / 2)
         .attr("y", height - 10) // Adjust this value to position the text
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
-    
         .text(totalCount);
 });
