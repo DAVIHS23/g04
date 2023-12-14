ccb362f7dfc3d7764ce28c936ae32aa1a9a163ad
Nilukzil <35670802+Nilukzil@users.noreply.github.com>
2023-12-14 17:14:46 +0100

    Update KPI.js

--- KPI.js
+++ KPI.js
@@ -1,52 +1,79 @@
-var margin = {
-        top: 30,
-        right: 30,
-        bottom: 70,
-        left: 60
-    },
-    width = 200 - margin.left - margin.right,
-    height = 200 - margin.top - margin.bottom;
+const margin = {
+    top: 30,
+    right: 30,
+    bottom: 70,
+    left: 60
+};
 
-// append the svg object to the body of the page
-var svg = d3.select("#kpi_nilu")
+const width = 200 - margin.left - margin.right;
+const height = 200 - margin.top - margin.bottom;
+
+// Append the SVG object to the body of the page
+const svg = d3.select("#kpi_nilu")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 svg.append("rect")
     .attr("width", width)
     .attr("height", height)
-    .attr("fill", "#f2f2f2"); 
+    .attr("fill", "#f2f2f2");
+
 // Parse the Data
-d3.csv("Merged_CSV.csv", function (data) {
-    
- var maxValue = "MAX Delay in Min: "+  d3.max(data, function(d) { return +d.DELAY_IN_MIN; });
+d3.csv("Merged_CSV.csv").then(data => {
+
+    const maxDelayFlight = d3.max(data, d => +d.DELAY_IN_MIN);
+    const maxDelayEntry = data.find(d => +d.DELAY_IN_MIN === maxDelayFlight);
+
+    const tooltip = d3.select("body").append("div")
+        .attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("visibility", "hidden");
+
+    const maxValue = "MAX Delay in Min: " + maxDelayFlight;
 
     // X axis
-    var x = d3.scaleBand()
+    const x = d3.scaleBand()
         .range([0, width])
         .domain(["DELAY_IN_MIN"]) // Only one value in the domain
         .padding(0.2);
 
-
-
     // Bars
-   svg.append("text")
+    svg.append("text")
         .attr("x", x("DELAY_IN_MIN") + x.bandwidth() / 2)
         .attr("y", height - 10) // Adjust this value to position the text
         .attr("text-anchor", "middle")
         .style("font-size", "16px")
-        
         .text(maxValue);
-    
-    var clickableDiv = d3.select("#kpi_nilu");
 
-// Add a click event listener
-clickableDiv.on("click", function() {
-  alert("Div Clicked!");
-});
+    const circles = svg.selectAll(".circle-group")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("class", "circle-group")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
+
+    circles.append("circle")
+        .attr("cx", x("DELAY_IN_MIN") + x.bandwidth() / 2) // Adjust the x position as needed
+        .attr("cy", height / 2) // Adjust the y position as needed
+        .attr("r", 5)
+        .attr("fill", "steelblue")
+        .on("mouseover", handleMouseOver)
+        .on("mouseout", handleMouseOut);
+
+    function handleMouseOver(event, d) {
+        // Update tooltip content and position
+        tooltip.html("Flight with the highest delay: " + maxDelayEntry.FLIGHT + ", Delay: " + maxDelayFlight + " minutes")
+            .style("left", (event.pageX + 10) + "px")
+            .style("top", (event.pageY - 10) + "px")
+            .style("visibility", "visible");
+    }
+
+    function handleMouseOut() {
+        // Hide the tooltip on mouseout
+        tooltip.style("visibility", "hidden");
+    }
 });
